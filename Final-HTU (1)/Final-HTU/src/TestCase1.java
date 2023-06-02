6270a27a96366f0e718c23fe8a41aac38c930260
Ahmmad Abu Shanab <108186197+ahmadabushanab@users.noreply.github.com>
2023-06-02 20:49:30 +0300

    TestCase1.java

--- Final-HTU (1)/Final-HTU/src/TestCase1.java
+++ Final-HTU (1)/Final-HTU/src/TestCase1.java
@@ -11,18 +11,9 @@ public class TestCase1 extends Parameters {
 	@BeforeTest
 	public  void beforeTest() {
 		driver.manage().window().maximize();
-		driver.get(URLS[1]);
+		driver.get("https://www.almosafer.com");
 			 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-				 try{
-					 driver.switchTo().frame("wiz-iframe-intent");
-						driver.findElement(By.xpath("//*[@id=\"contentDiv\"]/div/div/span")).click();
-						driver.switchTo().defaultContent();
-						driver.findElement(By.xpath("//*[@id=\"moe-dontallow_button\"]")).click();
-					    }
-					  catch(Exception e){
-							driver.findElement(By.xpath("//*[@id=\"moe-dontallow_button\"]")).click();
-					  	}				
-					driver.get(URLS[1]);
+				
 
 	} 
 	
